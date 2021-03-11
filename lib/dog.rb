f826220b3d6ba9604c0af4d562b858801feee0e5
Clayon Stewart <c.stewart89@gmail.com>
2021-03-11 03:10:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,10 +7,26 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
   
   def self.all
     @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+  
+  private
+  
+  def self.save
+    @@all << self
   end
 end
